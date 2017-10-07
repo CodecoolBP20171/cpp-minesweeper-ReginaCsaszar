7fd0eb89bc4a6147da839d987fc4e78c0ee2ecd4
ReginaCsaszar <regina.csaszar@gmail.com>
2017-10-07 20:00:14 +0200

    Step 1, 2, 3 done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <random>
 
 namespace {
     class Minesweeper {
     public:
-        Minesweeper(const size_t width, const size_t height)
-                : width(width), height(height),
+        Minesweeper(const size_t width, const size_t height, const int bomb)
+                : width(width), height(height), mines(bomb),
                   table(new char[width * height]) {
             fillTable();
         }
@@ -17,31 +18,84 @@ namespace {
          * We will learn about them later
          */
 
+
+
         virtual ~Minesweeper() {
             delete[] table;
         }
 
         void countNeighbours() {
-            // step 2 goes here
+
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    if (table[y*width+x] != '*') {
+                        int mineCount = 0;
+                        if (table[(y-1)*width+x] == '*') mineCount++;
+                        if (table[(y+1)*width+x] == '*') mineCount++;
+                        if (x > 0) {
+                            if (table[(y-1)*width+x-1] == '*') mineCount++;
+                            if (table[y*width+x-1] == '*') mineCount++;
+                            if (table[(y+1)*width+x-1] == '*') mineCount++;
+                        }
+                        if (x < width-1) {
+                            if (table[(y-1)*width+x+1]== '*') mineCount++ ;
+                            if (table[y*width+x+1]== '*') mineCount++ ;
+                            if (table[(y+1)*width+x+1]== '*') mineCount++ ;
+                        }
+                        table[y*width+x] = mineCount +'0';
+                    }
+                }
+            }
         }
 
         void printTable() const {
-            // step 3 goes here
+            std::cout<<"\nThe table content:\n\n";
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    std::cout <<table[y*width+x]<<" ";
+                }
+                std::cout<<std::endl;
+            }
+
         }
 
     private:
         void fillTable() {
-            // step 1 goes here
+            for(int i = 0; i < height*width; i++)
+            {
+                    table[i]  = '.';
+            }
+
+
+            std::random_device rd;
+            std::mt19937_64 gen(rd());
+
+            std::uniform_int_distribution<size_t> randIndex(0, width*height);
+
+            for(int i = 0; i < mines; i++)
+            {
+                size_t m = randIndex(gen);
+                if(table[m] == '*') {
+                    i--;
+                } else {
+                    table[m] = '*';
+                }
+            }
         }
 
         const size_t width, height;
+        const int mines;
         char *table;
     };
 }
 
 int main() {
     try {
-        Minesweeper ms(100, 50);
+        Minesweeper ms(10, 10, 15);
         ms.printTable();
         ms.countNeighbours();
         ms.printTable();
